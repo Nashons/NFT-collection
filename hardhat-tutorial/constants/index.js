2f73309bbdc571f09359a2f2f7563939495b44aa
agate <agatenashons@gmail.com>
2022-06-15 09:24:42 +0300

    Signed-off-by: agate <agatenashons@gmail.com>

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x347Fc7BD33A8b3676145C019Da6554989d8735d3";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
